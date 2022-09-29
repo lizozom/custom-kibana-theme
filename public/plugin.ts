9f44fea54c5f1e027b078e0a58801336fa4cdae9
lizozom <liza.katz@elastic.co>
2022-09-29 11:28:56 +0300

    Add a unique class to each app page

--- public/plugin.ts
+++ public/plugin.ts
@@ -6,9 +6,24 @@ export interface CustomKibanaLogoPluginContract {}
 export class CustomKibanaLogoPlugin
   implements Plugin<CustomKibanaLogoPluginContract, CustomKibanaLogoPluginContract>
 {
+  private curAppClass?: string;
+
+  constructor() {
+    console.log('Loaded CustomKibanaLogoPlugin');
+  }
+
   public setup(core: CoreSetup): CustomKibanaLogoPluginContract {
+    // Registration is done here so not to miss the first event
+    core.getStartServices().then(start => {
+
+      // Set a custom class to the body to scope css changes
+      start[0].application.currentAppId$.subscribe(currApp => {
+        this.setBodyClass(currApp)
+      })
+
+    })
     return {};
-  }                                                             
+  }
 
   public start(core: CoreStart): CustomKibanaLogoPluginContract {
     // You can place programmatic hooks here
@@ -16,4 +31,18 @@ export class CustomKibanaLogoPlugin
   }
 
   public stop() {}
+
+  private setBodyClass(curApp?: string) {
+    // Remove old class name
+    if (this.curAppClass) {
+      document.body.classList.remove(this.curAppClass);
+      this.curAppClass = undefined;
+    }
+
+    // Add new class name
+    if (curApp) {
+      this.curAppClass = `ckl-app-${curApp}`
+      document.body.classList.add(this.curAppClass);
+    }
+  }
 }
